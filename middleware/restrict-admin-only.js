277c6cbe325fc74fc2d2693121062424ce9508e8
Josh Stabback <josh.stabback@gmail.com>
2019-03-17 07:58:25 -0400

    Restrict to certain user ids

--- middleware/restrict-admin-only.js
+++ middleware/restrict-admin-only.js
@@ -8,8 +8,16 @@ export default async function ({ app, error }) {
       const provider = new firebase.auth.GoogleAuthProvider();
       const result = await app.$fireAuth.signInWithPopup(provider)
     } catch(e) {
-      return error({statusCode: 403, message: 'Access denied'})
+      return error({statusCode: 403, message: 'Access denied, not logged in'})
     }
+  }
+
+  const ADMINS = [
+    'h7df735HirfdPwdg9adCitD3f4l2',
+    'WO46v7wTeae5ub2muOgJVtX63iA2'
+  ]
+  if(!ADMINS.includes(app.$fireAuth.currentUser.uid)) {
+    return error({statusCode: 403, message: 'Access denied, not an admin'})
   }
 
   return null
