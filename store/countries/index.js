a0c5a699ea35c162c5d45e48a3b3ad50d1d81b0d
Josh Stabback <josh.stabback@gmail.com>
2019-03-17 08:06:32 -0400

    Rebase issues

--- store/countries/index.js
+++ store/countries/index.js
@@ -10,26 +10,6 @@ export const mutations = {
   ...commonStore.mutations
 }
 
-export const actions = {
-  async fetchCountries({commit})  {
-    const countriesRef = this.$fireDb.ref('countries')
-    try {
-      const snapshot = await countriesRef.once('value')
-      const countries = snapshot.val()
-      commit('saveCountries', Object.values(countries))
-    } catch (e) {
-      alert("Having some tech difficulties, try reloading")
-      return
-    }
-  }
-}
-
-export const mutations = {
-  saveCountries(s, payload) {
-    s.countries = payload
-  }
-}
-
 export const getters = {
   ...commonStore.getters
 }
--- store/steps/categories/index.js
+++ store/steps/categories/index.js
@@ -10,26 +10,6 @@ export const mutations = {
   ...commonStore.mutations
 }
 
-export const actions = {
-  async fetchCategories({commit})  {
-    const categoriesRef = this.$fireDb.ref('steps/categories')
-    try {
-      const snapshot = await categoriesRef.once('value')
-      const categories = snapshot.val()
-      commit('saveCategories', Object.values(categories))
-    } catch (e) {
-      alert("Having some tech difficulties, try reloading")
-      return
-    }
-  }
-}
-
-export const mutations = {
-  saveCategories(s, payload) {
-    s.categories = payload
-  }
-}
-
 export const getters = {
   ...commonStore.getters,
   itemBySlug: (_, getters) => slug => getters.itemByKey('slug', slug)
